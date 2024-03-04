67cc872b543dc54404e455508ab2989ad2af2e13
ryota <kurokawa.ryota.1124@gmail.com>
2024-03-04 16:57:08 +0900

    test2

---
+++ joppy2/user.swift
@@ -0,0 +1,14 @@
+//
+//  user.swift
+//  joppy2
+//
+//  Created by 黒川良太 on 2024/03/04.
+//
+
+import Foundation
+
+struct user: Codable {
+    let name: Int
+    let age: String
+    let user_id: String
+}
