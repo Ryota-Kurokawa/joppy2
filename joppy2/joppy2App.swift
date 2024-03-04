3739c5b970ac5451e26bb7bc846635e37325f476
ryota <kurokawa.ryota.1124@gmail.com>
2024-03-04 16:25:26 +0900

    initialization

---
+++ joppy2/joppy2App.swift
@@ -0,0 +1,17 @@
+//
+//  joppy2App.swift
+//  joppy2
+//
+//  Created by 黒川良太 on 2024/03/04.
+//
+
+import SwiftUI
+
+@main
+struct joppy2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ joppy2UITests/joppy2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  joppy2UITestsLaunchTests.swift
+//  joppy2UITests
+//
+//  Created by 黒川良太 on 2024/03/04.
+//
+
+import XCTest
+
+final class joppy2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
