59b9314d48f85e3176d0cbe25b862827bb5d1775
ryota <kurokawa.ryota.1124@gmail.com>
2024-03-06 02:00:05 +0900

    firebase initialization

---
+++ joppy2/Model/Publisher.swift
@@ -0,0 +1,14 @@
+//
+//  Publisher.swift
+//  joppy2
+//
+//  Created by 黒川良太 on 2024/03/06.
+//
+
+import Foundation
+
+struct Publisher: Codable, Identifiable {
+    var id: UUID = UUID()
+    let publisherName: String
+    let member: [UserInfo]
+}
---
+++ joppy2/Presentation/LoginScreen.swift
@@ -0,0 +1,18 @@
+//
+//  LoginScreen.swift
+//  joppy2
+//
+//  Created by 黒川良太 on 2024/03/06.
+//
+
+import SwiftUI
+
+struct LoginScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginScreen()
+}
