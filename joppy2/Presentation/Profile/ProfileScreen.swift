2499031733a314c74a52a83a8f63db21cae8f7d5
ryota <kurokawa.ryota.1124@gmail.com>
2024-03-13 02:39:55 +0900

    仮コミット

--- joppy2/Presentation/Profile/ProfileScreen.swift
+++ joppy2/Presentation/Profile/ProfileScreen.swift
@@ -9,7 +9,20 @@ import SwiftUI
 
 struct ProfileScreen: View {
     var body: some View {
-        Text("It is Profile Page!")
+        NavigationView {
+            VStack {
+                Image("profile")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                    .shadow(radius: 10)
+                    .padding(.bottom, 50)
+                Text("Ryota Kurokawa")
+                    .font(.title)
+                    .padding(.bottom, 30)
+            }
+        }
     }
 }
 
