55003176a2e7082cf1ef304dcae824e7cfc54a91
orukahairuka <sakuraierika03301109@gmail.com>
2024-03-16 19:54:52 +0900

    wip

---
+++ joppy2/Presentation/Event/Message/MessageElement.swift
@@ -0,0 +1,17 @@
+//
+//  MessageElement.swift
+//  joppy2
+//
+//  Created by 櫻井絵理香 on 2024/03/16.
+//
+
+import Foundation
+import FirebaseFirestoreSwift
+
+
+struct MessageElement: Codable {//jsonからswiftへ
+    @DocumentID var id: String?
+    var name: String
+    var message: String
+    var createAt: Date
+}
