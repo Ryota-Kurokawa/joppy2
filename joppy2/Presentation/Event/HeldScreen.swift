72a666e1f10d5f3d260efb4ca97ec5efcb5437ad
orukahairuka <sakuraierika03301109@gmail.com>
2024-03-17 19:47:08 +0900

    キーボードのフォーカス調整

--- joppy2/Presentation/Event/HeldScreen.swift
+++ joppy2/Presentation/Event/HeldScreen.swift
@@ -35,6 +35,7 @@ struct EditView: View {
     @State var typeDescription = ""
     @State private var HeldDate: Date = Date() // 現在の日付と時刻で初期化
     @State private var keyboardHeight: CGFloat = 0 // キーボードの高さを保持する状態
+    @FocusState private var isEditControll: Bool
 
 
     var body: some View {
@@ -47,7 +48,12 @@ struct EditView: View {
                     isShowSheet = false
                 }) {
                     Text("投稿する")
+                        .foregroundStyle(.white)
+                        .fontWeight(.semibold)
                 }
+                .frame(width: 120, height: 60)
+                                    .background(Color.orange)
+                                    .cornerRadius(30.0)
             DatePicker(
                         "日付と時刻を選択",
                         selection: $HeldDate,
@@ -55,37 +61,51 @@ struct EditView: View {
                     )
                     .padding()
             TextField("タイトル", text: $typeTitle)
+                .focused($isEditControll)
+                .onTapGesture {
+                    isEditControll = false
+                }
                 .textFieldStyle(.roundedBorder)
                 .frame(width: 355,height: 40)
                 .padding(.all)
+
             TextEditor(text: $typeDescription)
-                .frame(width: 355,height: 380)
+                .focused($isEditControll)
+                .onTapGesture {
+                    isEditControll = false//フォーカス解除
+                }
+                .frame(width: 355,height: 285)
                 .overlay(
                     RoundedRectangle(cornerRadius: 5) // 角が丸い四角形をオーバーレイとして追加
-                        .stroke(.gray, lineWidth: 1) // このオーバーレイに枠線を適用
+                        .stroke(Color.gray, lineWidth: 1) // このオーバーレイに枠線を適用
                 )
+                .padding(.bottom,20)
+
 
         }
         .padding(.all)
-        .offset(x: 0, y: keyboardHeight / 2) // キーボードの高さに応じてオフセット調整
-        .onAppear {
-                    self.registerKeyboardNotifications()
-                }
-                .onDisappear {
-                    NotificationCenter.default.removeObserver(self)
-                }
+//        .offset(x: 0, y: keyboardHeight / 2) // キーボードの高さに応じてオフセット調整
+//        .onAppear {
+//                    self.registerKeyboardNotifications()
+//                }
+//                .onDisappear {
+//                    NotificationCenter.default.removeObserver(self)
+//                }
     }
-    // キーボードの表示・非表示を監視するためのメソッド
-       private func registerKeyboardNotifications() {
-           NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { notification in
-               guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
-               self.keyboardHeight = keyboardFrame.height
-           }
-
-           NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { _ in
-               self.keyboardHeight = 0
-           }
-       }
+//    // キーボードの表示・非表示を監視するためのメソッド
+//       private func registerKeyboardNotifications() {
+//           NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { notification in
+//               guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+//               self.keyboardHeight = keyboardFrame.height
+//           }
+//
+//           NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { _ in
+//               self.keyboardHeight = 0
+//           }
+//       }
 }
 
 
+#Preview {
+    EditButton(isShowSheet: .constant(true))
+}
